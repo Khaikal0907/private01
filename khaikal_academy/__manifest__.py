f5a7e027816b2d64a6b145eea5fdac554a2b41e2
khaikalaz1@gmail.com <khaikalaz1@gmail.com>
2022-12-11 15:00:46 +0700

    act and menu

--- khaikal_academy/__manifest__.py
+++ khaikal_academy/__manifest__.py
@@ -24,10 +24,11 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'data/data_course.xml',
         'views/views.xml',
         'views/templates.xml',
+        'views/course.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
