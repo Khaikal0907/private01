5d19d02f15e326d679dc581e3b36e24ff50b4ae6
husni0204 <husnisale@gmail.com>
2022-12-17 13:55:58 +0700

    rubah

--- khaikal_academy/models/course.py
+++ khaikal_academy/models/course.py
@@ -5,9 +5,22 @@ from odoo import models, fields, api
 
 class Course(models.Model):
     _name = 'khaikal_academy.course'
+    _description
 
-    name = fields.Char(string='Title', required=True)
-    description = fields.Text(string='Description')
+    name = fields.Char(
+        string='Course Name',
+        required=True,
+        help='Fill course name....'
+        )
+
+    description = fields.Text(
+        string='Description'
+        )
+
+    active = fields.Boolean(
+        string='Active', 
+        default=True
+        )
 
     # @api.depends('value')
     # def _value_pc(self):
